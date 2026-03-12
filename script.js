88142206bc2ea970efa310c598d3792d22df2e62
gameone5310-collab <gameone5310@gmail.com>
2026-03-12 20:54:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function buyProduct(name, price){
+
+var options = {
+"key": "YOUR_RAZORPAY_KEY",
+"amount": price * 100,
+"currency": "INR",
+"name": "Creator Digital Store",
+"description": name,
+
+handler: function (response){
+alert("Payment Successful!");
+
+window.location.href="download.html";
+}
+
+};
+
+var rzp = new Razorpay(options);
+rzp.open();
+
+}
